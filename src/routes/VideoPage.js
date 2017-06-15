3a234cd2eb3992fb9779b08943ae52dd857d5ec1
马戬宇 <wb-mjy275902@antfin.com>
2017-06-15 20:24:51 +0800

    'yk'

--- src/routes/VideoPage.js
+++ src/routes/VideoPage.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { connect } from 'dva';
 import { createForm } from 'rc-form';
-
 import styles from './VideoPage.less';
 import Footer from './footer.less';
 
@@ -22,6 +21,40 @@ class VideoPage extends React.Component {
       this.props.dispatch({ type: 'Index/getNewLesson'});
     }
   }
+
+  componentDidMount() {
+    $(function () {
+      player = new YKU.Player('youkuplayer', {
+        styleid: '0',
+        client_id: 'ce68c2e986caab6e',
+        vid: 'XMTg5NjQ5MDY5Ng==',
+        newPlayer: true,
+        autoplay: false
+      });
+    });
+  }
+  myReplace(str){
+    if(str){
+      return str.replace(/\\n/g,'\n').
+      replace(/\\r/g,'\r').
+      replace(/\\t/g,'\t').
+      replace(/\&amp;/g,'&').
+      replace(/\&quot;/g,'"').
+      replace(/\&lt;/g,'<').
+      replace(/\&gt;/g,'>').
+      replace(/\&hellip;/g,'...').
+      replace(/\&mdash;/g,'--').
+      replace(/\&nbsp;/g,' ').
+      replace(/\&copy;/g,'©').
+      replace(/\&middot;/g,'·').
+      replace(/\&#39;/g,"'").
+      replace(/\&ldquo;/g,'“').
+      replace(/\&rdquo;/g,'”').
+      replace(/\&lsquo;/g,'‘').
+      replace(/\&rsquo;/g,'’').
+      replace(/\\\\;/g,'\\');
+    }
+  };
   render() {
     const Item = this.props.Index.lessonData.data;
     const lessonList = this.props.Index.lessonList;
@@ -32,37 +65,38 @@ class VideoPage extends React.Component {
         <div id="youkuplayer" className={styles.video_play} style={{ width: '100%', height: '180px' }} />
         <div className={styles.wrap96}>
 
-          <div className={styles.vd_name} id="session_name">{ Item.eventName ? Item.eventName: '' }</div>
+          <div className={styles.vd_name} id="session_name">{ Item.eventName ? this.myReplace(Item.eventName): '' }</div>
 
           <div className={styles.vd_title}>最新课程</div>
           <div id="sessionList">
             {
-              lessonList?lessonList.map((item, index)=>{
-
+              lessonList?lessonList.map((item)=>{
+                  return (
+                    <a className={styles.vd_item} href={`#/video?eventId=${item.eventId?item.eventId:''}&type=${type}`}>
+                      <img
+                        alt=""
+                        src={item.imageI}
+                        className={styles.photo}
+                      />
+                      <div className={styles.txt}>
+                        {this.myReplace(item.eventName)}
+                      </div>
+                      <div className={styles.clear} />
+                    </a>
+                  );
                 }):''
             }
-            <a className={styles.vd_item}>
-              <img
-                alt=""
-                src="https://jinmajiang.oss-cn-beijing.aliyuncs.com/image/20161215112325_2137.jpg"
-                className={styles.photo}
-              />
-              <div className={styles.txt}>
-                今蚂讲智慧医疗专场-天猫内容运营分享
-              </div>
-              <div className={styles.clear} />
-            </a>
           </div>
         </div>
         <div className={Footer.footer} id="footer">
-          <a href={`#/?eventId=${ this.props.location.query.eventId}`} className={Footer.hover}>活动简介</a>
+          <a href={`#/?eventId=${ this.props.location.query.eventId}`} >活动简介</a>
           <a href={`#/item?eventId=${ this.props.location.query.eventId}`}>活动议程</a>
           {
             type == 'event'? (
                 Item.status == 'FINISHED'?<a href="javascript:void(0)" id="bm" className={Footer.a3}>已结束</a>:
-                  <a href={`#/register?eventId=${ this.props.location.query.eventId}&type=${type}`}>立即报名</a>
+                  <a href={`#/register?eventId=${ this.props.location.query.eventId}&type=${type}`} className={Footer.hover}>立即报名</a>
               ):
-              <a href={`#/video?eventId=${Item.eventId?Item.eventId:''}&type=${type}`}>在线观看</a>
+              <a href={`#/video?eventId=${Item.eventId?Item.eventId:''}&type=${type}`} className={Footer.hover}>在线观看</a>
           }
         </div>
         <div className={Footer.footer_zw} />
