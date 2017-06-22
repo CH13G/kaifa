3202be0b7a0bda3d0602a6e4b3b2dc308d01dea1
马戬宇 <wb-mjy275902@antfin.com>
2017-06-22 22:01:35 +0800

    'm'

--- src/routes/VideoPage.js
+++ src/routes/VideoPage.js
@@ -46,15 +46,15 @@ class VideoPage extends React.Component {
   }
   createPlayer(vid){
     console.log('vid',vid);
-      // $(() => {
-      //     player = new YKU.Player('youkuplayer', {
-      //         styleid: '0',
-      //         client_id: 'ce68c2e986caab6e',
-      //         vid: vid,
-      //         newPlayer: true,
-      //         autoplay: false,
-      //     });
-      // });
+      $(() => {
+          player = new YKU.Player('youkuplayer', {
+              styleid: '0',
+              client_id: 'ce68c2e986caab6e',
+              vid: vid,
+              newPlayer: true,
+              autoplay: false,
+          });
+      });
   }
   myReplace(str) {
     if (str) {
