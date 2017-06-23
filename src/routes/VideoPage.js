ff792786c03a93604bc753304ac690594ddd66a0
马戬宇 <wb-mjy275902@antfin.com>
2017-06-23 16:32:59 +0800

    M

--- src/routes/VideoPage.js
+++ src/routes/VideoPage.js
@@ -20,7 +20,9 @@ class VideoPage extends React.Component {
       // hash值发生改变
       eventId = this.props.location.query.eventId || '';
       type = this.props.location.query.type || 'lesson';
-      this.createPlayer(this.props.location.query.vid);
+      if(this.props.location.path.indexOf('video')!==-1){
+        this.createPlayer(this.props.location.query.vid);
+      }
       this.props.dispatch({ type: 'Index/setState', lessonName: this.props.location.query.lessonName});
       for( let i = 0; i< this.props.Index.lessonList.length; i++){
         if( this.props.Index.lessonList[i].eventId == eventId){
@@ -54,7 +56,7 @@ class VideoPage extends React.Component {
             autoplay: false,
           });
       } catch ( e ) {
-          alert("player undefined ......",e)
+          // alert("player undefined ......",e)
       }
       //});
   }
